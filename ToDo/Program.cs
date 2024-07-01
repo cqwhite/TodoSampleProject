3b2c253cee6f5ea62267dbded1dbda40ada1a492
Connor White <cqwhite19@gmail.com>
2024-07-01 14:31:20 -0400

    adding cors policy

--- ToDo/Program.cs
+++ ToDo/Program.cs
@@ -7,10 +7,22 @@ builder.Services.AddControllers();
 builder.Services.AddScoped<ITaskItemRepository, TaskItemRepository>();
 builder.Services.AddScoped<ITaskItemService, TaskItemService>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 
 app.UseHttpsRedirection();
 app.MapControllers();
+app.UseCors("AllowFrontend");
+
 
 app.Run();
